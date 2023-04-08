466be490a1448474daec29b8e6cee458979b687b
Juram Lee <juram2@naver.com>
2023-04-08 21:55:09 +0900

    :sparkles:Feat: Reducer

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 #### 👉 구현 예정 기능 (stack, library ...)
 
-TailwindCSS, react-hook-form, pagination(정석 / react-js-pagination), 검색엔진, 로그인로직, recoil,  
-redux, reducer, react-query, serverless배포, suspense, redux-tool-kit, InfiniteScroll(정석), 마크다운 언어 변환,  
-CSS 테이블/그리드, Storybook, bootstrap, mediaQuery(react-device-detect(라이브러리), react-responsive(라이브러리),  
+TailwindCSS, react-hook-form, pagination(정석 / react-js-pagination), 검색엔진, 로그인로직, recoil, redux, reducer,  
+react-query, serverless배포, suspense, redux-tool-kit, InfiniteScroll(정석), 마크다운 언어 변환, CSS 테이블/그리드,  
+Storybook, bootstrap, 반응형-react-device-detect(라이브러리), react-responsive(라이브러리),  
 <br></br>
 
 #### 👉 토이 프로젝트로 사용해본 기능
---
+++ Toy-project/issue/src/reducer/issues.js
@@ -0,0 +1,67 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import IssueApi from 'apis/issueApi';
+
+// value
+const initialState = {
+  issues: [],
+  targetIssue: {},
+  getTodoState: {
+    loading: false,
+    done: false,
+    err: null,
+  },
+};
+
+// reducer
+export const issueSlice = createSlice({
+  name: 'issue',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getIssues.pending, (state) => {
+      state.getTodoState.loading = true;
+    });
+
+    builder.addCase(getIssues.fulfilled, (state, action) => {
+      state.issues = action.payload;
+      state.getTodoState.loading = false;
+      state.getTodoState.done = true;
+      state.getTodoState.err = null;
+    });
+
+    builder.addCase(getIssues.rejected, (state, action) => {
+      state.getTodoState.loading = false;
+      state.getTodoState.done = true;
+      state.getTodoState.err = action.payload;
+    });
+
+    builder.addCase(getTargetIssue.pending, (state) => {
+      state.getTodoState.loading = true;
+    });
+
+    builder.addCase(getTargetIssue.fulfilled, (state, action) => {
+      state.targetIssue = action.payload;
+      state.getTodoState.loading = false;
+      state.getTodoState.done = true;
+      state.getTodoState.err = null;
+    });
+
+    builder.addCase(getTargetIssue.rejected, (state, action) => {
+      state.getTodoState.loading = false;
+      state.getTodoState.done = true;
+      state.getTodoState.err = action.payload;
+    });
+  },
+});
+
+export const getIssues = createAsyncThunk('issue/getIssues', async (issue) => {
+  const res = await IssueApi.getIssueList(issue);
+  return res.data;
+});
+
+export const getTargetIssue = createAsyncThunk(
+  'issue/getTargetIssue',
+  async (id) => {
+    const res = await IssueApi.getIssue(id);
+    return res.data;
+  }
+);
