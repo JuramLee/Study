6bb659b6884d65abcee1fdfff43e851b38eb03ff
Juram Lee <juram2@naver.com>
2023-04-22 11:28:40 +0900

    :tada:Feat: react-hook-form studying initiation

---
+++ Study/hookform/src/signUp.js
@@ -0,0 +1,10 @@
+const SignUp = () => {
+  return (
+    <div>
+      <input id="email" placeholder="email"></input>
+      <input id="pw" placeholder="password"></input>
+    </div>
+  );
+};
+
+export default SignUp;
