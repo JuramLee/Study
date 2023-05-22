1e3eedfe872124e71829d2664c86ac4f12521786
Juram Lee <juram2@naver.com>
2023-05-22 23:29:36 +0900

    주소찾기 지도랑 연결

--- Study/address/src/matchingMap.js
+++ Study/address/src/matchingMap.js
@@ -1,5 +1,42 @@
+import { useState } from 'react';
+import { Map } from 'react-kakao-maps-sdk';
+
+const { kakao } = window;
+
 const matchingMap = () => {
-  // 테스트
+  const [state, setState] = useState({
+    center: { lat: 37.496768, lng: 127.02474 },
+    inPanto: true,
+  });
+
+  const [searchAddress, SetSearchAddress] = useState();
+
+  const searchMap = () => {
+    const geocoder = new kakao.maps.services.Geocoder();
+
+    let callback = function (result, status) {
+      if (status === kakao.maps.services.Status.OK) {
+        const newSearch = result[0];
+        console.log(newSearch);
+        setState({
+          center: { lat: newSearch.y, lng: newSearch.x },
+        });
+      }
+    };
+    geocoder.addressSearch(`${searchAddress}`, callback);
+  };
+
+  const handleSearchAddress = (e) => {
+    SetSearchAddress(e.target.value);
+  };
+
+  return (
+    <Map
+      center={state.center}
+      isPanto={state.isPanto}
+      style={{ width: '100%', height: '360px' }}
+      level={3}></Map>
+  );
 };
 
 export default matchingMap;
