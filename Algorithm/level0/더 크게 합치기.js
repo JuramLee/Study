15df20ebe8dbaa677f83e370e695c6bd8770c4f2
Juram Lee <juram2@naver.com>
2023-06-30 00:06:38 +0900

    알고리즘 문제 풀이

---
+++ Algorithm/level0/더 크게 합치기.js
@@ -0,0 +1,20 @@
+/*
+연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+
+12 ⊕ 3 = 123
+3 ⊕ 12 = 312
+양의 정수 a와 b가 주어졌을 때, a ⊕ b와 b ⊕ a 중 더 큰 값을 return 하는 solution 함수를 완성해 주세요.
+
+단, a ⊕ b와 b ⊕ a가 같다면 a ⊕ b를 return 합니다.
+*/
+
+function solution(a, b) {
+  var answer = 0;
+  const option1 = String(a) + String(b);
+  const option2 = String(b) + String(a);
+  if (Number(option1) > Number(option2) || Number(option1) == Number(option2)) {
+    answer = Number(option1);
+  } else answer = Number(option2);
+
+  return answer;
+}
