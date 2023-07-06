273c2273692c1e2b71da703cca011f4cd64c9a90
Juram Lee <juram2@naver.com>
2023-07-06 23:17:45 +0900

    typescript day02

---
+++ Study/type/src/practice.ts
@@ -0,0 +1,18 @@
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return 'Hello ' + person.firstName + ' ' + person.lastName;
+}
+
+let user = { firstName: 'June', lastName: 'User' };
+
+document.body.textContent = greeter(user);
+
+let obj: object = { name: 'NAME', age: 29 };
+
+obj = { A: 'A', B: 'B' };
+
+export {};
