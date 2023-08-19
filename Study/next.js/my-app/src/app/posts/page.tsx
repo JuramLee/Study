952b8a0039e9cee7c422dfb6b936ef627b91dfbd
Juram Lee <juram2@naver.com>
2023-08-19 23:54:01 +0900

    :ambulance: fetch 데이터를 tsx로 뿌릴 때 오류

---
+++ Study/next.js/my-app/src/app/posts/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface IPosts {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+}
+
+export default function PostsMain() {
+  const [posts, setPosts] = useState<IPosts[]>();
+  // let posts: Array<IPosts> = [];
+  const getData = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((result) => {
+        setPosts(result);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(posts);
+
+  return (
+    <div>
+      <h2>posts 페이지</h2>
+      {posts &&
+        posts.map((post) => {
+          <div>{post.id}</div>;
+        })}
+    </div>
+  );
+}
